bcf3354054db0cede3c79f99c7ab937f5ca39b03
lekoal <coreglus@gmail.com>
2022-01-23 16:35:12 +0300

    Data receiving migrated into another class

--- app/src/main/java/com/example/cininfo/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/cininfo/ui/main/MainFragment.kt
@@ -20,6 +20,7 @@ import com.example.cininfo.data.FilmOuterDTO
 import com.example.cininfo.data.ResultURL
 import com.example.cininfo.databinding.MainFragmentBinding
 import com.example.cininfo.logic.AppState
+import com.example.cininfo.logic.FilmDataReceiver
 import com.example.cininfo.logic.FilmItemRecyclerAdapter
 import com.example.cininfo.logic.MainViewModel
 import com.google.android.material.snackbar.Snackbar
@@ -38,11 +39,6 @@ class MainFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var viewModel: MainViewModel
-
-    private val freshUrl =
-        "https://api.themoviedb.org/3/discover/movie?api_key=${BuildConfig.TMDB_API_KEY}&language=ru-RU&sort_by=release_date.desc&primary_release_year=2021"
-    private val popularUrl =
-        "https://api.themoviedb.org/3/discover/movie?api_key=${BuildConfig.TMDB_API_KEY}&language=ru-RU&sort_by=popularity.desc"
 
     companion object {
         fun newInstance() = MainFragment()
@@ -77,12 +73,14 @@ class MainFragment : Fragment() {
         val loadingLayout = binding.loadingLayout
         val mainView = binding.mainView
 
+        FilmDataReceiver.getFilmsServer()
+
         loadingLayout.apply {
             when (appState) {
                 is AppState.Success -> {
                     val filmData = appState.filmData
                     visibility = View.GONE
-                    getFilmsServer(freshUrl, popularUrl)
+                    setData(FilmDataReceiver.getFreshList(), FilmDataReceiver.getPopularList())
                     showSnackBar(R.string.snack_bar_success_text)
                 }
                 is AppState.Loading -> {
@@ -155,57 +153,5 @@ class MainFragment : Fragment() {
             viewModel.getFilmDataFromRemoteSource()
             renderData(appState)
         }.show()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getLines(reader: BufferedReader) = reader.lines().collect(Collectors.joining("/n"))
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getFilmsServer(myFreshUrl: String, myPopularUrl: String) {
-
-        try {
-            val freshUri =
-                URL(myFreshUrl)
-            val popularUri =
-                URL(myPopularUrl)
-            val handler = Looper.myLooper()?.let { Handler(it) }
-            Thread(Runnable {
-                lateinit var freshUrlConnection: HttpsURLConnection
-                lateinit var popularUrlConnection: HttpsURLConnection
-                try {
-                    val freshResultURL: ResultURL = makeConnection(freshUri)
-                    val popularResultURL: ResultURL = makeConnection(popularUri)
-                    freshUrlConnection = freshResultURL.getConnection()
-                    popularUrlConnection = popularResultURL.getConnection()
-                    val freshList = freshResultURL.getList()
-                    val popularList = popularResultURL.getList()
-                    handler?.post { setData(freshList, popularList) }
-                } catch (e: Exception) {
-                    Log.e("", "Fail connection", e)
-                    e.printStackTrace()
-                } finally {
-                    freshUrlConnection.disconnect()
-                    popularUrlConnection.disconnect()
-                }
-            }).start()
-        } catch (e: MalformedURLException) {
-            Log.e("", "Fail URI", e)
-            e.printStackTrace()
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun makeConnection(uri: URL): ResultURL {
-        val urlConnection = uri.openConnection() as HttpsURLConnection
-        urlConnection.requestMethod = "GET"
-        urlConnection.readTimeout = 10000
-        val bufferedReader =
-            BufferedReader(InputStreamReader(urlConnection.inputStream))
-        val filmList = Gson().fromJson(
-            getLines(bufferedReader),
-            FilmOuterDTO::class.java
-        ).results
-
-        return ResultURL(filmList, urlConnection)
     }
 }
