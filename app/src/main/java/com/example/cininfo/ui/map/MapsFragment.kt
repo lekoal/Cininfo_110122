a6350513391876ce3a63fed2bcc26c745d14e650
lekoal <coreglus@gmail.com>
2022-02-11 15:46:30 +0300

    Geofences of cinema & notification of enter of cinema areas is added

--- app/src/main/java/com/example/cininfo/ui/map/MapsFragment.kt
+++ app/src/main/java/com/example/cininfo/ui/map/MapsFragment.kt
@@ -2,16 +2,27 @@ package com.example.cininfo.ui.map
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
+import android.location.*
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.cininfo.R
 import com.example.cininfo.databinding.FragmentMapsBinding
+import com.example.cininfo.logic.GeofenceBroadcastReceiver
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 
 import com.google.android.gms.maps.GoogleMap
@@ -20,19 +31,51 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.io.IOException
+
+private const val REFRESH_PERIOD = 1000L
+private const val MINIMAL_DISTANCE = 1f
 
 class MapsFragment : Fragment(), CoroutineScope by MainScope() {
 
     private lateinit var map: GoogleMap
     private var menu: Menu? = null
     private val markers: ArrayList<Marker> = ArrayList()
-    private var _binding : FragmentMapsBinding? = null
+    private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
+    private val permissionResult =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                getLocation()
+            } else {
+                Toast.makeText(
+                    context,
+                    getString(R.string.dialog_message_no_gps),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private val onLocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            context?.let {
+                getAddressAsync(it, location)
+            }
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+
+    }
+
+    private var geofenceList: MutableList<Geofence> = mutableListOf()
+
+    private val geofenceRadius = 100f
+
+    private lateinit var geofencingClient: GeofencingClient
 
     @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
@@ -41,14 +84,26 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
         map.uiSettings.isZoomControlsEnabled = true
         map.uiSettings.isMyLocationButtonEnabled = true
 
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             map.isMyLocationEnabled = true
         }
     }
 
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
+        PendingIntent.getBroadcast(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        geofencingClient = LocationServices.getGeofencingClient(requireActivity())
+
     }
 
     companion object {
@@ -69,12 +124,21 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
         initSearchByAddress()
+
+        checkPermission()
+
+        addAddresses()
+
+        addGeofence()
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        removeGeofence()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -84,7 +148,7 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.menu_map_mode_normal -> {
                 map.mapType = GoogleMap.MAP_TYPE_NORMAL
                 return true
@@ -132,5 +196,124 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
                 .position(location)
                 .title(searchText)
         )?.let { markers.add(it) }
+    }
+
+    private fun addAddress(latitude: Double, longitude: Double, id: String) {
+        geofenceList.add(
+            Geofence.Builder()
+                .setRequestId(id)
+                .setCircularRegion(latitude, longitude, geofenceRadius)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .build()
+        )
+    }
+
+    private fun addAddresses() {
+        addAddress(69.02374065510082, 33.083954982555426, "Аврора")
+        addAddress(68.9681022688139, 33.09759865767168, "Мурманск")
+        addAddress(68.95814584116252, 33.06965233567983, "Мираж Синема")
+        addAddress(68.96199054137912, 33.11071052823777, "Галактика")
+        addAddress(68.92225468981877, 33.11690895784426, "Кронверк Синема")
+    }
+
+    private fun checkPermission() {
+        activity?.let {
+            when (PackageManager.PERMISSION_GRANTED) {
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    getLocation()
+                }
+                else -> {
+                    requestPermission()
+                }
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        permissionResult.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        context?.let { context ->
+            val locationManager =
+                context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    REFRESH_PERIOD,
+                    MINIMAL_DISTANCE,
+                    onLocationListener
+                )
+            } else {
+                val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (location == null) {
+                    Toast.makeText(
+                        context,
+                        getString(R.string.dialog_title_gps_turned_off),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    getAddressAsync(context, location)
+                }
+            }
+        }
+    }
+
+    private fun getAddressAsync(context: Context, location: Location) {
+        val geoCoder = Geocoder(context)
+        launch {
+            val job = async(Dispatchers.IO) {
+                geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+            }
+            val addresses = job.await()
+//            showAddressesDialog(addresses[0].getAddressLine(0), location)
+        }
+    }
+
+    private fun showAddressesDialog(address: String, location: Location) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_address_title))
+                .setMessage(address)
+                .setPositiveButton(getString(R.string.dialog_address_get_cinema)) { _, _ ->
+                    // Вызов показа кинотеатров
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun seekGeofencing(): GeofencingRequest {
+        return GeofencingRequest.Builder().apply {
+            setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            addGeofences(geofenceList)
+        }.build()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addGeofence() {
+        geofencingClient.addGeofences(seekGeofencing(), geofencePendingIntent).run {
+            addOnSuccessListener {
+                Toast.makeText(requireContext(), "Geofence(s) added", Toast.LENGTH_SHORT).show()
+            }
+            addOnFailureListener {
+                Toast.makeText(requireContext(), "Failed to add geofence(s)", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun removeGeofence(){
+        geofencingClient.removeGeofences(geofencePendingIntent).run {
+            addOnSuccessListener {
+                Toast.makeText(requireContext(), "Geofences removed", Toast.LENGTH_SHORT).show()
+            }
+            addOnFailureListener {
+                Toast.makeText(requireContext(), "Failed to remove geofences", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
